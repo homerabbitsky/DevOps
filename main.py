dce2b7fa573d5896b995a128c77ab59ba06b8afd
Skylar Thornton <90333080+homerabbitsky@users.noreply.github.com>
2022-11-30 17:52:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+''' example module w math added '''
+import unittest
+
+def square(n):
+  ''' returns the square of the perameter '''
+  return n*n
+
+def cube(n):
+  ''' returns the cube of the perameter '''
+  return n*n*n
+
